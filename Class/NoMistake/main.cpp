d24d59d69bbd672ab438efcbe4ebbbb2892764be
ryandeleon1 <ryandeleon2@gmail.com>
2016-01-05 11:46:45 -0800

    Signed-off-by: ryandeleon1 <ryandeleon2@gmail.com>

---
+++ Class/NoMistake/main.cpp
@@ -0,0 +1,33 @@
+/* 
+    Author: Ryan DeLeon
+    Created on January 5, 2016, 11:15 AM
+    Purpose: No Mistake
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+// Function prototype
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    short  c,b=20000, a=30000;
+    
+    //Calculate or map inputs to outputs
+    c=a+b;
+    
+    //0utput the results
+    cout<<"a = "<<a<<endl;
+    cout<<"b = "<<b<<endl;
+    cout<<c<<"="<<a <<"+"<< b<<endl;
+            
+    // And done!
+    return 0;
+}
+
---
+++ Homework/Assignment 1/Sacitch_8thED_Chap1_Prob10/main.cpp
@@ -0,0 +1,38 @@
+/* 
+    Author: Ryan DeLeon
+    Created on January 5, 2016, 11:40 AM
+    Purpose: Big C
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+// Function prototype
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    char letter='X';
+    
+    //Calculate or map inputs to outputs
+    
+    //0utput the results
+    cout<<"  "<<letter<<letter<<letter<<endl;
+    cout<<" "<<letter<<"   "<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<" "<<letter<<"   "<<letter<<endl; 
+     cout<<"  "<<letter<<letter<<letter<<endl;
+            
+    // And done!
+    return 0;
+}
+
