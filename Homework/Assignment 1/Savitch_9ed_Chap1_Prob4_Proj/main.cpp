040b2e26bbe479c3e7f79845af68218bd5bd4402
ryandeleon1 <ryandeleon2@gmail.com>
2016-01-07 11:40:07 -0800

    Signed-off-by: ryandeleon1 <ryandeleon2@gmail.com>

--- Homework/Assignment 1/Savitch_9ed_Chap1_Prob4_Proj/main.cpp
+++ Homework/Assignment 1/Savitch_9ed_Chap1_Prob4_Proj/main.cpp
@@ -10,6 +10,7 @@
 #include <cmath>   //Math functions
 #include <ctime>   //Time to set the random speed
 #include <iomanip> //Format library
+#include <fstream> //Reading/writing to a file
 using namespace std;
 
 //User Libraries
@@ -21,23 +22,33 @@ const float GRAVITY=32.174f;//Sea level earth acceleration constant in ft/sec^2
 
 //Execution Begins Here
 int main(int argc, char** argv) {
+    
     //Set the random number seed
     srand(static_cast<unsigned int>(time(0))) ;  //
     
     //Declare Variables
-    float drpTime, distance; //Time in second distance in feet
+    ofstream out; //Declare a file stream object called out
+    float drpTime, dist; //Time in second, distance in feet
     
-    //inputting th time with a random number
+    //inputting the time with a random number and opening a file
     drpTime=rand()%11+10; //[10,20]secs
+    const int SIZE=21;
+    char fileNam[SIZE]="DropDistance.dat";
+    out.open(fileNam);
     
-    //Calculate the 
-    distance=0.5*GRAVITY*drpTime*drpTime;
+    //Calculate the distance dropped
+    dist=0.5*GRAVITY*drpTime*drpTime;
     
     //Output the results
     cout<<"Drop Time = "<<drpTime<<" sec"<<endl;
-    cout<<fixed<<setprecision(3)<<showpoint<<endl;
-    cout<<"Distance dropped = "<<distance<<" ft"<<endl;
+    cout<<fixed<<setprecision(3)<<showpoint<<endl; //format the distance to 3 decimals
+    cout<<"Distance dropped ="<<setw(9)<<dist<<" ft"<<endl;
+    
+    //Output the results to a file
+    out<<fixed<<setprecision(3)<<showpoint<<endl;
+    out<<"Distance dropped ="<<setw(9)<<dist<<" ft"<<endl;
 
-    //All Done!
+    //All Done! make sure to close files
+    out.close();
     return 0;
 }
