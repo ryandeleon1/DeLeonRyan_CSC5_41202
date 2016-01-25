ae4550563a492e8abea78404546df701bdf595a5
ryandeleon2@gmail.com <ryandeleon1>
2016-01-25 08:15:54 -0800

    Signed-off-by: ryandeleon2@gmail.com <ryandeleon1>

--- Homework/Assignment 3/Menu With Problems-Decisions/main.cpp
+++ Homework/Assignment 3/Menu With Problems-Decisions/main.cpp
@@ -33,6 +33,7 @@ int main(int argc, char** argv) {
     cout<<"Type 3 to display Savitch 8thEd Chap3 Prob 12."<<endl;
     cout<<"Type 4 to display Savitch 7thEd Chap3 Prob 1."<<endl;
     cout<<"Type 5 to display Savitch 9thEd Chap3 Prob 3."<<endl;
+    cout<<"Type 6 to display Savitch 9thEd Chap3 Prob3 Practice."<<endl;
     cout<<"Type anything else to exit."<<endl;
     cin>>nSoltn;
     
@@ -244,6 +245,80 @@ int main(int argc, char** argv) {
                      case 2: cout<<"I";
                      case 1: cout<<"I";
                  }
+            }case 6:{
+                    //Declare and initialize variables
+                unsigned char answer;
+                unsigned int month, day; //Month and day of birthday
+                bool reDsply;
+                cout<<endl<<"Savitch 9thEd Chap3 Prob3."<<endl;
+                cout<<"Zodiac sign indicator."<<endl;
+
+                do{
+                    //input the users birth date
+                    cout<<"What is the month of your birthday? (Enter as a number 1-12)"<<endl;
+                    cin>>month;
+                    cout<<"Now enter the day."<<endl;
+                    cin>>day;
+
+                    //determine the sign
+                    switch(month){
+                        case(12):{
+                        if (day>=22) cout<<"Your zodiac sign is Capricorn."<<endl;
+                        else cout<<"Your zodiac sign is Sagittarius"<<endl; 
+                        break;}
+                        case(1):{
+                        if (day>=20){ cout<<"Your zodiac sign is Aquarius."<<endl;}
+                        else cout<<"Your zodiac sign is Capricorn."<<endl;
+                        break;}
+                        case(2):{
+                        if (day>=19) cout<<"Your zodiac sign is Pisces."<<endl;
+                        else cout<<"Your zodiac sign is Aquarius."<<endl;
+                        break;}
+                        case(3):{
+                        if (day>=21) cout<<"Your zodiac sign is Aries."<<endl;
+                        else cout<<"Your zodiac sign is Pisces."<<endl; 
+                        break;}
+                        case(4):{
+                        if (day>=20){ cout<<"Your zodiac sign is Taurus."<<endl;}
+                        else cout<<"Your zodiac sign is Aries."<<endl;
+                        break;}
+                        case(5):{
+                        if (day>=21) cout<<"Your zodiac sign is Gemini."<<endl;
+                        else cout<<"Your zodiac sign is Taurus."<<endl;
+                        break;}
+                        case(6):{
+                        if (day>=22) cout<<"Your zodiac sign is Cancer."<<endl;
+                        else cout<<"Your zodiac sign is Gemini."<<endl; 
+                        break;}
+                        case(7):{
+                        if (day>=23){ cout<<"Your zodiac sign is Leo."<<endl;}
+                        else cout<<"Your zodiac sign is Cancer."<<endl;
+                        break;}
+                        case(8):{
+                        if (day>=23) cout<<"Your zodiac sign is Virgo."<<endl;
+                        else cout<<"Your zodiac sign is Leo."<<endl;
+                        break;}
+                        case(9):{
+                        if (day>=23) cout<<"Your zodiac sign is Libra."<<endl;
+                        else cout<<"Your zodiac sign is Virgo."<<endl; 
+                        break;}
+                        case(10):{
+                        if (day>=23){ cout<<"Your zodiac sign is Scorpio."<<endl;}
+                        else cout<<"Your zodiac sign is Libra."<<endl;
+                        break;}
+                        case(11):{
+                        if (day>=22) cout<<"Your zodiac sign is Sagittarius."<<endl;
+                        else cout<<"Your zodiac sign is Scorpio."<<endl;
+                        break;}
+
+                    }
+                    cout<<"Would you like to try again?"<<endl;
+                    cin>>answer;
+                    if (answer='y')
+                        reDsply=true;
+                    else 
+                        reDsply=false;
+                }while (reDsply=true);
             }
             default:{
                 cout<<endl<<"Exiting the program"<<endl;
--- Homework/Assignment 3/Savitch_9thEd_Chap3_Prob3_Pract/main.cpp
+++ Homework/Assignment 3/Savitch_9thEd_Chap3_Prob3_Pract/main.cpp
@@ -18,8 +18,9 @@ using namespace std;
 //Execution begins here
 int main(int argc, char** argv) {
     //Declare and initialize variables
-    unsigned char month, day, answer; //Month and day of birthday
-    bool reDsply=true;
+    unsigned char answer;
+    unsigned int month, day; //Month and day of birthday
+    bool reDsply;
     cout<<endl<<"Savitch 9thEd Chap3 Prob3."<<endl;
     cout<<"Zodiac sign indicator."<<endl;
     
@@ -31,29 +32,63 @@ int main(int argc, char** argv) {
         cin>>day;
         
         //determine the sign
-        if (month=12){
+        switch(month){
+            case(12):{
             if (day>=22) cout<<"Your zodiac sign is Capricorn."<<endl;
-            else cout<<"Your zodiac sign is Sagittarius"<<endl;      
-    }else if (month=1){
-        if (day>=20){ cout<<"Your zodiac sign is Aquarius."<<endl;}
-        else cout<<"Your zodiac sign is Capricorn."<<endl;
-    }else if (month==2){
-        if (day>=19) cout<<"Your zodiac sign is Pisces."<<endl;
-        else cout<<"Your zodiac sign is Aquarius."<<endl;
-    }else if (month==3){
-        if (day>=21) cout<<"Your zodiac sign is Aries."<<endl;
-        else cout<<"Your zodiac sign is Pisces."<<endl;
-    }else if (month==4){
-        if (day>=20) cout<<"Your zodiac sign is Taurus."<<endl;
-        else cout<<"Your zodiac sign is Aries."<<endl;
-    }
+            else cout<<"Your zodiac sign is Sagittarius"<<endl; 
+            break;}
+            case(1):{
+            if (day>=20){ cout<<"Your zodiac sign is Aquarius."<<endl;}
+            else cout<<"Your zodiac sign is Capricorn."<<endl;
+            break;}
+            case(2):{
+            if (day>=19) cout<<"Your zodiac sign is Pisces."<<endl;
+            else cout<<"Your zodiac sign is Aquarius."<<endl;
+            break;}
+            case(3):{
+            if (day>=21) cout<<"Your zodiac sign is Aries."<<endl;
+            else cout<<"Your zodiac sign is Pisces."<<endl; 
+            break;}
+            case(4):{
+            if (day>=20){ cout<<"Your zodiac sign is Taurus."<<endl;}
+            else cout<<"Your zodiac sign is Aries."<<endl;
+            break;}
+            case(5):{
+            if (day>=21) cout<<"Your zodiac sign is Gemini."<<endl;
+            else cout<<"Your zodiac sign is Taurus."<<endl;
+            break;}
+            case(6):{
+            if (day>=22) cout<<"Your zodiac sign is Cancer."<<endl;
+            else cout<<"Your zodiac sign is Gemini."<<endl; 
+            break;}
+            case(7):{
+            if (day>=23){ cout<<"Your zodiac sign is Leo."<<endl;}
+            else cout<<"Your zodiac sign is Cancer."<<endl;
+            break;}
+            case(8):{
+            if (day>=23) cout<<"Your zodiac sign is Virgo."<<endl;
+            else cout<<"Your zodiac sign is Leo."<<endl;
+            break;}
+            case(9):{
+            if (day>=23) cout<<"Your zodiac sign is Libra."<<endl;
+            else cout<<"Your zodiac sign is Virgo."<<endl; 
+            break;}
+            case(10):{
+            if (day>=23){ cout<<"Your zodiac sign is Scorpio."<<endl;}
+            else cout<<"Your zodiac sign is Libra."<<endl;
+            break;}
+            case(11):{
+            if (day>=22) cout<<"Your zodiac sign is Sagittarius."<<endl;
+            else cout<<"Your zodiac sign is Scorpio."<<endl;
+            break;}
+        }
         cout<<"Would you like to try again?"<<endl;
         cin>>answer;
         if (answer='y')
             reDsply=true;
         else 
             reDsply=false;
-    }while (reDsply);
+    }while (reDsply=true);
     
     
     // And done!
